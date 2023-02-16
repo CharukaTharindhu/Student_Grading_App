62bd45911226acdf6dd5e2d2b57bc2e80b01f33c
charukatharindhu <charukatharindhu@gmail.com>
2023-02-16 15:19:06 +0530

    Added Fixed fetures

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -9,18 +9,18 @@ export class StudentService {
   constructor(private _http: HttpClient) {}
 
   addStudent(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+    return this._http.post('https://localhost:7011/api/controller', data);
   }
 
   updateStudent(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+    return this._http.put(`https://localhost:7011/api/controller/${id}`, data);
   }
 
   getStudentList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get('https://localhost:7011/api/controller');
   }
 
   deleteStudent(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+    return this._http.delete(`https://localhost:7011/api/controller/${id}`);
   }
 }
