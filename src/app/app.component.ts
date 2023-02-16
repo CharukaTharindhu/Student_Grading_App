4a8dba9d6a48484479c76bea785ef150faa57594
charukatharindhu <charukatharindhu@gmail.com>
2023-02-16 15:04:10 +0530

    Added my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { StudentAddEditComponent } from './student-add-edit/student-add-edit.component';
+import { StudentService } from './services/student.service';
+import { CoreService } from './core/core.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'student-grading-app';
+
+  displayedColumns: string[] = [
+    'name',
+    'dob',
+    'gender',
+    'subject',
+    'mark',
+    'grade',
+    'remark',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private _dialog: MatDialog,
+    private _empService: StudentService,
+    private _coreService: CoreService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  openAddEditEmpForm() {
+    const dialogRef = this._dialog.open(StudentAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      },
+    });
+  }
+
+  getEmployeeList() {
+    this._empService.getStudentList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: number) {
+    this._empService.deleteStudent(id).subscribe({
+      next: (res) => {
+        this._coreService.openSnackBar('Employee deleted!', 'done');
+        this.getEmployeeList();
+      },
+      error: console.log,
+    });
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(StudentAddEditComponent, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      },
+    });
+  }
 }
---
+++ src/app/student-add-edit/student-add-edit.component.ts
@@ -0,0 +1,86 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
+import { StudentService } from '../services/student.service';
+
+@Component({
+  selector: 'app-student-add-edit',
+  templateUrl: './student-add-edit.component.html',
+  styleUrls: ['./student-add-edit.component.scss'],
+})
+export class StudentAddEditComponent implements OnInit {
+  empForm: FormGroup;
+  items: any[] = [];
+  grade: string = '';
+
+  addSubject() {
+    this.items.push({
+      index: this.items.length + 1,
+      value: 0,
+      grade: '',
+    });
+  }
+  marksOnChange(index: number) {
+    alert(this.items[index].value + ' ' + index); // this.items[index].value
+    if (this.items[index].value >= 0 && this.items[index].value <= 35) {
+      this.items[index].grade = 'F';
+    } else if (this.items[index].value > 35 && this.items[index].value <= 55) {
+      this.items[index].grade = 'C';
+    } else if (this.items[index].value > 55 && this.items[index].value <= 75) {
+      this.items[index].grade = 'B';
+    } else if (this.items[index].value > 75 && this.items[index].value <= 100) {
+      this.items[index].grade = 'A';
+    }
+  }
+
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: StudentService,
+    private _dialogRef: MatDialogRef<StudentAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      name: '',
+      gender: '',
+      dob: '',
+      subject: '',
+      mark: '',
+      grade: '',
+      remark: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this._empService
+          .updateStudent(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              this._coreService.openSnackBar('Employee detail updated!');
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.error(err);
+            },
+          });
+      } else {
+        this._empService.addStudent(this.empForm.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Employee added successfully');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }
+    }
+  }
+}
